a9017d30118ba4394d6783cb880235d196f1c4c3
k33g <ph.charriere@gmail.com>
2025-06-25 09:46:38 +0200

    🛟 Updated.

--- agents/main.py
+++ agents/main.py
@@ -7,7 +7,7 @@ from google.adk.cli.fast_api import get_fast_api_app
 # Get the directory where main.py is located
 AGENT_DIR = os.path.dirname(os.path.abspath(__file__))
 # Example session DB URL (e.g., SQLite)
-SESSION_DB_URL = "sqlite:///./sessions.db"
+#SESSION_DB_URL = "sqlite:///./sessions.db"
 # Example allowed origins for CORS
 ALLOWED_ORIGINS = ["http://localhost", "http://localhost:8080","http://localhost:12435","http://localhost:5050",  "*"]
 # Set web=True if you intend to serve a web interface, False otherwise
@@ -16,7 +16,7 @@ SERVE_WEB_INTERFACE = True
 # Call the function to get the FastAPI app instance
 app: FastAPI = get_fast_api_app(
     agents_dir=AGENT_DIR,
-    session_db_url=SESSION_DB_URL,
+    #session_db_url=SESSION_DB_URL,
     allow_origins=ALLOWED_ORIGINS,
     web=SERVE_WEB_INTERFACE,
 )
